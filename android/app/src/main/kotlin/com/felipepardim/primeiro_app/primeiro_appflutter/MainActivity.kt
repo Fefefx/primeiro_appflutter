8109858af82df8b1a60287956f23efb7efac78cb
Felipe Mantoan Pardim <felipemantoan@hotmail.com>
2023-07-15 17:48:37 -0300

    fix(directory): fixed directory page

---
+++ android/app/src/main/kotlin/com/felipepardim/primeiro_app/primeiro_appflutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.felipepardim.primeiro_app.primeiro_appflutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- primeiro_app_flutter/android/app/src/main/kotlin/com/felipepardim/primeiro_app/primeiro_app_flutter/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.felipepardim.primeiro_app.primeiro_app_flutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
